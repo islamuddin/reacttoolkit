c482205bf4f3cd15d8bb32e2a3a50b3ca8890feb
Syed Hamza Raza <hamzaraza409@gmail.com>
2023-12-31 17:11:59 +0500

    counter decrement

--- src/features/counter/Counter.jsx
+++ src/features/counter/Counter.jsx
@@ -1,6 +1,6 @@
 import React, { useState} from 'react'
 import { useSelector, useDispatch } from 'react-redux'
-import { decrement, increment, incrementByAmount } from './counterSlice'
+import { decrement, increment, incrementByAmount, decrementByAmount } from './counterSlice'
 
 export default function Counter() {
   const count = useSelector(state => state.counter.value)
@@ -8,41 +8,59 @@ export default function Counter() {
   const [input, setInput] = useState();
   return (
     <div>
-      <div>        
+      <div>
         <button
           aria-label="Increment value"
           onClick={() => {
-            if (count === 100)
-            {
-              console.log("Counter limit is 100")
+            if (count === 100) {
+              console.log("Counter limit is 100");
+            } else {
+              dispatch(increment());
             }
-            else{
-              dispatch(increment())
-            }
-              
-          } }
+          }}
         >
           Increment
         </button>
         <input
           aria-label="Amount"
-          value={input}          
+          value={input}
           onChange={(e) => setInput(e.target.value)}
-        >          
-        </input>
-        <button        
-          onClick={(e) => dispatch(incrementByAmount(input)) }
-        >
-          Amount
+        ></input>
+        <button onClick={(e) => {
+          if (count === 100) {
+            console.log("Counter cannot be greater than 100");
+          } else {
+            dispatch(incrementByAmount(input));
+          }
+        }
+            }>
+          Amount Increment
+        </button>
+
+        <button onClick={(e) => {
+          if (count === 0){
+            console.log("Counter cannot be less than 0")
+          }
+          else{
+            dispatch(decrementByAmount(input))
+          }          
+        }}>
+          Amount Decrement
         </button>
         <span>{count}</span>
         <button
           aria-label="Decrement value"
-          onClick={() => dispatch(decrement())}
+          onClick={() => {
+            if (count == 0) {
+              console.log("Counter Minimun limit is 0");
+            } else {
+              dispatch(decrement());
+            }
+          }}
         >
           Decrement
         </button>
       </div>
     </div>
-  )
+  );
 }
