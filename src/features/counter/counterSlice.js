d9653cc51716349e9aabef2a5a4e5760a55aed24
islamuddin <uddinislam46@gmail.com>
2023-12-31 16:09:19 +0500

    react toolkit counter example

---
+++ src/features/counter/counterSlice.js
@@ -0,0 +1,32 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+
+// step 1: create a slice
+// todo state update, change, action 
+// todo object => property, function
+export const counterSlice = createSlice({
+    name: "counter",
+    initialState: { value: 0 },
+    reducers: {
+        increment: (state) => {
+            state.value += 1
+        },
+        decrement: (state) => {
+            state.value -= 1
+          },
+        incrementByAmount: (state, action) => {
+            state.value += action.payload
+        }
+    }
+
+});
+
+// step 2: export the action
+// todo explicitly export functions from the slice so that we can use in components
+export const { increment, decrement, incrementByAmount } = counterSlice.actions;
+
+// step 3: export the reducer: just for the store to recognize as a reducer
+export default counterSlice.reducer;
+
+
+
